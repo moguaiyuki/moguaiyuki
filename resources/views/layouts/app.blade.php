5ddd5a4e36a6074f87ce3e3cb5503696c50b397b
moguaiyuki <moguai.yuki0424@gmail.com>
2018-05-03 18:16:37 +0900

    modify migration files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,8 +43,8 @@
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                         {{--登録フォーム--}}
-                            {{--<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>--}}
-                        @else
+                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        @els e
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
